4abc084e32e3c60e248b65954d870a0547273eb2
João Moreira de Sá Coutinho <joao.moreiradsc@gmail.com>
2021-04-08 00:19:28 +0200

    Coded sample3 (fundamentals/rectangles) and sample4 (translation).

--- sample3/main.js
+++ sample3/main.js
@@ -21,14 +21,13 @@ void main() {
 `
 // Fragment shader GLSL code
 let fragment_shader_2d = `
-// Fragment shaders don't have a defaut precision, so we need to pick
-// one. medium is a good default.
 precision mediump float;
-    
+
+// Make the fragment shader take a color uniform input
+uniform vec4 u_color;
+
 void main() {
-    // gl_FragColor is a special variable a fragment shader is responsible for
-    // setting
-    gl_FragColor = vec4(1, 0, 0.5, 1); // return reddsih-purple
+    gl_FragColor = u_color;
 }
 `
 // Create a shader, upload the GLSL source, and compile the source
@@ -60,6 +59,34 @@ function createProgram(gl, vsh_src, fsh_src) {
     gl.deleteProgram(p)
 }
 
+// Return a random int from 0 to range - 1
+function randomInt(range) {
+    return Math.floor(Math.random()*range)
+}
+
+// Fill the buffer with the values that define a rectangle
+function setRectangle(gl, x, y, width, height) {
+    let x1 = x
+    let x2 = x + width
+    let y1 = y
+    let y2 = y + height
+
+    // Note: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect whatever buffer is
+    // bound to the `ARRAY_BUFFER` bind point. So far we only have one buffer,
+    // but if we had more than one, we'd want ot bind it to `ARRAY_BUFFER`
+    // first.
+
+    let positions = [
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+}
+
 function main() {
     //--------------------------------------------------------------------------
     // Initialization code
@@ -78,25 +105,14 @@ function main() {
     // Look up the location of the attribute
     let a_loc = gl.getAttribLocation(p, "a_pos")
 
-    // Look up the location of the uniform
+    // Look up the location of the uniforms
     let u_loc = gl.getUniformLocation(p, "u_resolution")
+    let u_color_loc = gl.getUniformLocation(p, "u_color")
 
     // Create a buffer, and bind it (think ARRAY_BUFFER = buf)
     let buf = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, buf)
     
-    // Put data (six 2d points) into the buffer
-    let positions = [
-        10, 20,
-        80, 20,
-        10, 30,
-        10, 30,
-        80, 20,
-        80, 30,
-    ]
-    // Transform the JavaScript array into strongly typed data
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
     //--------------------------------------------------------------------------
     // Rendering code
     //--------------------------------------------------------------------------
@@ -142,9 +158,18 @@ function main() {
     // uniform2f function sets a value for it, as a couple of lfoats.
     gl.uniform2f(u_loc, gl.canvas.width, gl.canvas.height)
     
-    // We can finally ask WebGL to execute our GLSL program and draw
-    let count = 6
-    gl.drawArrays(gl.TRIANGLES, offset, count)
+    // Draw 50 random rectangles in random colors
+    for(let ii=0; ii<50; ++ii) {
+	// Setup a random rectangle. This will write to buf because it's the
+	// last thing we bound on the ARRAY_BUFFER bind point.
+	setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300))
+
+	// Set a random color
+	gl.uniform4f(u_color_loc, Math.random(), Math.random(), Math.random(), 1)
+	
+	let count = 6
+	gl.drawArrays(gl.TRIANGLES, offset, count)
+    }
 }
 
 main()
--- sample4/main.js
+++ sample4/main.js
@@ -21,14 +21,13 @@ void main() {
 `
 // Fragment shader GLSL code
 let fragment_shader_2d = `
-// Fragment shaders don't have a defaut precision, so we need to pick
-// one. medium is a good default.
 precision mediump float;
-    
+
+// Make the fragment shader take a color uniform input
+uniform vec4 u_color;
+
 void main() {
-    // gl_FragColor is a special variable a fragment shader is responsible for
-    // setting
-    gl_FragColor = vec4(1, 0, 0.5, 1); // return reddsih-purple
+    gl_FragColor = u_color;
 }
 `
 // Create a shader, upload the GLSL source, and compile the source
@@ -60,6 +59,34 @@ function createProgram(gl, vsh_src, fsh_src) {
     gl.deleteProgram(p)
 }
 
+// Return a random int from 0 to range - 1
+function randomInt(range) {
+    return Math.floor(Math.random()*range)
+}
+
+// Fill the buffer with the values that define a rectangle
+function setRectangle(gl, x, y, width, height) {
+    let x1 = x
+    let x2 = x + width
+    let y1 = y
+    let y2 = y + height
+
+    // Note: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect whatever buffer is
+    // bound to the `ARRAY_BUFFER` bind point. So far we only have one buffer,
+    // but if we had more than one, we'd want ot bind it to `ARRAY_BUFFER`
+    // first.
+
+    let positions = [
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+}
+
 function main() {
     //--------------------------------------------------------------------------
     // Initialization code
@@ -75,32 +102,37 @@ function main() {
     // Call our function to create shaders and link them into a program
     let p = createProgram(gl, vertex_shader_2d, fragment_shader_2d)
 
-    // Look up the location of the attribute
+    // Look up the location of the attribute (where the vertex data needs to go)
     let a_loc = gl.getAttribLocation(p, "a_pos")
 
-    // Look up the location of the uniform
+    // Look up the location of the uniforms
     let u_loc = gl.getUniformLocation(p, "u_resolution")
+    let u_color_loc = gl.getUniformLocation(p, "u_color")
 
     // Create a buffer, and bind it (think ARRAY_BUFFER = buf)
     let buf = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+
+    // New in sample 4
+    let translation = [0, 0]
+    let width = 100
+    let height = 30
+    let color = [Math.random(), Math.random(), Math.random(), 1]
     
-    // Put data (six 2d points) into the buffer
-    let positions = [
-        10, 20,
-        80, 20,
-        10, 30,
-        10, 30,
-        80, 20,
-        80, 30,
-    ]
-    // Transform the JavaScript array into strongly typed data
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+    drawScene()
 
-    //--------------------------------------------------------------------------
-    // Rendering code
-    //--------------------------------------------------------------------------
+    // Setup a UI
+    webglLessonsUI.setupSlider("#x", {slide: updatePosition(0), max: gl.canvas.width})
+    webglLessonsUI.setupSlider("#y", {slide: updatePosition(1), max: gl.canvas.height})
 
+    function updatePosition(index) {
+	return function(event, ui){
+	    translation[index] = ui.value
+	    drawScene()
+	}
+    }
+
+function drawScene() {
     // See https://webglfundamentals.org/webgl/lessons/webgl-resizing-the-canvas.html
     webglUtils.resizeCanvasToDisplaySize(gl.canvas)
 
@@ -114,9 +146,6 @@ function main() {
     // Tell it to use our program (pair of shaders)
     gl.useProgram(p)
 
-    // Tell WebGL how to take data from the buffer we setup above, and supply
-    // it to the attribute in the shader.
-
     // First we need to turn the attribute on:
     gl.enableVertexAttribArray(a_loc)
 
@@ -126,6 +155,9 @@ function main() {
     // from initializing ?
     // // Bind the position buffer (again)
     // gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+
+    // Setup a rectangle
+    setRectangle(gl, translation[0], translation[1], width, height)
 
     // Tell the attribute how to get data out of buf (ARRAY_BUFFER)
     let size = 2          // 2 components per iteration
@@ -142,9 +174,13 @@ function main() {
     // uniform2f function sets a value for it, as a couple of lfoats.
     gl.uniform2f(u_loc, gl.canvas.width, gl.canvas.height)
     
-    // We can finally ask WebGL to execute our GLSL program and draw
+    // Set the color
+    gl.uniform4fv(u_color_loc, color)
+
+    // Draw the rectangle
     let count = 6
     gl.drawArrays(gl.TRIANGLES, offset, count)
+}
 }
 
 main()
